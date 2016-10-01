a46a22c92b053abe85eb9d7839356ac64b8e21ab
Daniel-Gallup <drust328@millardps.org>
2016-09-30 21:54:31 -0500

    Complete week 1 challenge
    Completed week 1 problems with bonuses.

--- scripts.js
+++ scripts.js
@@ -1,58 +1,128 @@
-function problemOne() {
-  //Get all the numbers from the form.
-  let num1 = parseFloat(document.getElementById('num1').value);
+function problemOne() 
+{
+	// Get all the numbers from the form.
 
-  //Find the average, not including the lowest score.
+	let scoreList = [];
 
-  //Print it to the console.
-  console.log(average);
+	for(let i = 1; i <= 6; i++)
+		scoreList.push(parseFloat(document.getElementById('num' + i).value));
+
+	// Get rid of NaNs from invalid input
+
+	scoreList = scoreList.map(function(v){ return isNaN(v) ? 0 : v; });
+
+	// Find the average, not including the lowest score.
+
+	let min = getMinIdx(scoreList);
+	let average = 0;
+
+	for(let i = 0; i < scoreList.length; i++)
+		if(min != i)
+			average += scoreList[i];
+	  
+	average /= scoreList.length - 1;
+
+	// Print it to the console.
+
+	console.log(average);
 }
 
-function problemTwo() {
-  //Get all the letter grades from the form.
-  let grade1 = document.getElementById('grade1').value;
+function problemOneBonus()
+{
+	// Split up the list of scores
 
-  //Compute the GPA.
+	scoreList = document.getElementById('bonusField').value.split(",");
 
-  //Print it to the console.
+	// Trim, and turn them into floats
 
-  //If it is 3.0 or above, turn the div with id="box" green. Otherwise, turn it red.
-  // document.getElementById("box").style.backgroundColor = "orange";
+	scoreList = scoreList.map(function(v){ return parseFloat(v.trim()); });
+	
+	// Find the average, not including the lowest score.
 
+	let min = getMinIdx(scoreList);
+	let average = 0;
+
+	for(let i = 0; i < scoreList.length; i++)
+		if(min != i)
+			average += scoreList[i];
+	  
+	average /= scoreList.length - 1;
+	
+	if(scoreList.length == 1)
+		average = scoreList[0];
+
+	// Print it to the console.
+
+	console.log(average);
 }
 
-function findMin(num1, num2, num3, num4, num5, num6) {
-  let list = [num1, num2, num3, num4, num5, num6];
-  let minimum = 0;
+function problemTwo()
+{
+	// Get all the letter grades from the form.
 
-  for (let i = 1; i < 6; i++) {
-    if(list[i] < list[minimum]) {
-      minimum = i;
-    }
-  }
+	let gradeList = [];
 
-  return list[minimum];
+	for(let i = 1; i <= 6; i++)
+		gradeList.push([document.getElementById('grade' + i).value, document.getElementById('gradeHonors' + i).checked]);
+	
+	// Compute the GPA.
+	
+	let GPA = getNumericalGPAFromLetter(gradeList);
+	
+	// Print it to the console.
+	
+	console.log(GPA);
+	
+	// If it is 3.0 or above, turn the div with id="box" green. Otherwise, turn it red.
+	
+	document.getElementById("box").style.backgroundColor = GPA >= 3.0 ? "green" : "red";
 }
 
-function findGPA(grade1, grade2, grade3, grade4, grade5, grade6) {
-  let list = [grade1, grade2, grade3, grade4, grade5, grade6];
+function getMinIdx(numList)
+{
+	let min = 0;
+	
+	for(let i = 0; i < numList.length; i++)
+		if(numList[i] < numList[min])
+			min = i;
+		
+	return min;
+}
 
-  let sum = 0;
-
-  for(let i = 0; i < 6; i++) {
-    if(list[i] == 'a' || list[i] == 'A') {
-      sum += 4;
-    }
-    else if(list[i] == 'b' || list[i] == 'B') {
-      sum += 3;
-    }
-    else if(list[i] == 'c' || list[i] == 'C') {
-      sum += 2;
-    }
-    else if(list[i] == 'd' || list[i] == 'D') {
-      sum += 1;
-    }
-  }
-
-  return sum / 6;
+function getNumericalGPAFromLetter(grades)
+{
+	let sum = 0;
+	
+	for(let i = 0; i < grades.length; i++)
+	{
+		let letter = grades[i][0]
+		
+		switch(true)
+		{
+			case letter == 'a' || letter == 'A':
+				sum += 4 + (grades[i][1] ? 1 : 0);
+				
+				break;
+				
+			case letter == 'b' || letter == 'B':
+				sum += 3 + (grades[i][1] ? 1 : 0);
+				
+				break;
+				
+			case letter == 'c' || letter == 'C':
+				sum += 2;
+				
+				break;
+				
+			case letter == 'd' || letter == 'D':
+				sum += 1;
+				
+				break;
+				
+			default:
+				break;
+		}
+	}
+	
+	return sum / grades.length;
 }
